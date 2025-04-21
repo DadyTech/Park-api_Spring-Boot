a1a1ccfcb12e1cb55e95cc7fe844a0b5ef6cbba2
Dady francisco mendes <dev_dady@outlook.com>
2025-04-21 20:18:41 -0300

    Configurar o Timezone e o Locale padrão

--- src/main/java/com/dev_dady/park_api/config/SpringTimeZoneConfig.java
+++ src/main/java/com/dev_dady/park_api/config/SpringTimeZoneConfig.java
@@ -1,6 +1,12 @@
 package com.dev_dady.park_api.config;
 
-public class SpringTimeZoneConfig {
+import jakarta.annotation.PostConstruct;
 
-    
+import java.util.TimeZone;
+
+public class SpringTimeZoneConfig {
+    @PostConstruct
+    public void timezoneConfig(){
+        TimeZone.setDefault(TimeZone.getTimeZone("America/Sao_Paulo"));
+    }
 }
