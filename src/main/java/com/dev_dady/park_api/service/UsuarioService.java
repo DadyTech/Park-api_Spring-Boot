8635b3a02bc6f76307781b832489f80b7bca8172
Dady francisco mendes <dev_dady@outlook.com>
2025-04-21 23:03:44 -0300

    Recurso para criar um novo usuário

--- src/main/java/com/dev_dady/park_api/service/UsuarioService.java
+++ src/main/java/com/dev_dady/park_api/service/UsuarioService.java
@@ -1,15 +1,21 @@
 package com.dev_dady.park_api.service;
 
+import com.dev_dady.park_api.model.entity.Usuario;
 import com.dev_dady.park_api.repository.UsuarioRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class UsuarioService {
 
     private final UsuarioRepository usuarioRepository;
-    
+
     public UsuarioService(UsuarioRepository usuarioRepository) {
         this.usuarioRepository = usuarioRepository;
+    }
+
+    @Transactional
+    public Usuario createUser(Usuario usuario){
+        return usuarioRepository.save(usuario);
     }
 }
--- src/main/java/com/dev_dady/park_api/web/UsuarioController.java
+++ src/main/java/com/dev_dady/park_api/web/UsuarioController.java
@@ -1,7 +1,10 @@
 package com.dev_dady.park_api.web;
 
+import com.dev_dady.park_api.model.entity.Usuario;
 import com.dev_dady.park_api.service.UsuarioService;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,5 +17,10 @@ public class UsuarioController {
 
     public UsuarioController(UsuarioService usuarioService) {
         this.usuarioService = usuarioService;
+    }
+    @PostMapping
+    public ResponseEntity<Usuario> createUser(Usuario usuario){
+        Usuario user = usuarioService.createUser(usuario);
+        return ResponseEntity.status(HttpStatus.CREATED).body(user);
     }
 }
